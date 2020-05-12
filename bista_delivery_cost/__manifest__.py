cd0874c4f6fafad7a471682e7fe74791c924ebaa
Jigar sorathiya <jigar.sorathiya@bistasolutions.com>
2020-05-12 14:20:48 +0530

    [ADD/UPDATE] updated the report design and added new module for delivery cost.

---
+++ bista_delivery_cost/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Bista Solutions Pvt. Ltd
+# Copyright (C) 2020 (https://www.bistasolutions.com)
+#
+##############################################################################
+
+{
+    'name': 'JMAC Delivery Cost',
+    'version': "13.0.1.0.1",
+    'author': 'Bista Solutions Pvt. Ltd.',
+    'website': "https://www.bistasolutions.com",
+    'category': 'Base',
+    'summary': """JMAC Delivery cost module help to make the calculation
+    of Delivery cost based on JMAC Rules.""",
+    'description': """
+    JMAC Delivery cost module help to make the calculation
+    of Delivery cost based on JMAC Rules.
+    """,
+    'depends': ['delivery','product'],
+    'data': [
+        'view/product_views.xml',
+    ],
+    'auto_install': False
+}
