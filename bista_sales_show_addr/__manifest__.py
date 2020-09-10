5db396a6fc8641f3db8505e2bce5efa1ae9e5dc9
Patrick Buller <patrick.buller@bistasolutions.com>
2020-09-10 15:41:30 -0500

    Override the _get_name function to get this to mostly work.

--- bista_sales_show_addr/__manifest__.py
+++ bista_sales_show_addr/__manifest__.py
@@ -7,16 +7,16 @@
 ##############################################################################
 
 {
-    'name': 'JMAC Sales Show Address',
-    'version': "13.0.0",
-    'author': 'Bista Solutions Pvt. Ltd.',
-    'website': "https://www.bistasolutions.com",
-    'category': 'Sales',
-    'summary': """On Sales Quote, displays the actual address.""",
-    'description': """
+    "name": "JMAC Sales Show Address",
+    "version": "13.0.0",
+    "author": "Bista Solutions Pvt. Ltd.",
+    "website": "https://www.bistasolutions.com",
+    "category": "Sales",
+    "summary": """On Sales Quote, displays the actual address.""",
+    "description": """
     On a sales quote, in the delivery and invoice addresses, will display the chosen full address, not the name of the partner address.
     """,
-    'depends': [],
-    'data': [],
-    'auto_install': False
+    "depends": [],
+    "data": [],
+    "auto_install": False,
 }
--- bista_sales_show_addr/models/res_partner.py
+++ bista_sales_show_addr/models/res_partner.py
@@ -10,11 +10,12 @@ from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    def address_get(self, adr_pref=None):
-        print(f">>>> Inside Extended Model")
-        partners = super(ResPartner, self).address_get(adr_pref)
-        print(f">>>> {partners}")
-        return partners
-        # res = super(SaleOrder, self).create(vals)
+    def _get_name(self):
+        if not self.type in ["delivery", "invoice"]:
+            return super(ResPartner, self)._get_name()
+        self = self.with_context(show_address_only=True)
+        self = self.with_context(show_address=False)
+        default_name = super(ResPartner, self)._get_name()
+        return " ".join(default_name.splitlines()[0:2])
