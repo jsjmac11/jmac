d8ef7f362791286f3e6431df7b5df806cce20624
shawaz <shawaz.jahangiri@bistasolutions.com>
2020-10-22 15:33:45 +0530

    [IMP] vendor pricelist enable ignore cost

--- sale_distributor/models/product.py
+++ sale_distributor/models/product.py
@@ -14,3 +14,14 @@ from odoo.tools import float_compare
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
+
+
+class ProductSupplierinfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    active = fields.Boolean("Active", default=True)
+    ignore_cost = fields.Boolean("Ignore Cost?", default=False)
+
+    @api.onchange('ignore_cost')
+    def onchange_ignore_cost(self):
+        self.active = False if self.ignore_cost else True
