496c211c8877b393d7b5753e9625b206007ac229
cheten.ravaliya <chetan.ravaliya@bistasolutions.com>
2020-11-28 18:46:40 +0530

    [Add/Update] Next and Previous functionality update, field value box from form view and save button issue

--- sale_distributor/static/src/js/next_prev_buttons.js
+++ sale_distributor/static/src/js/next_prev_buttons.js
@@ -29,6 +29,7 @@ odoo.define('sale.distributor', function (require) {
     var FormViewDialog = FormDialog.FormViewDialog.include({
         init: function (parent, options) {
             var res = this._super(parent, options);
+            var multi_select = !_.isNumber(options.res_id) && !options.disable_multiple_selection;
             if ((this.res_model == 'sale.order.line') && this.context.is_next_prev){
                 var options = this.options;
                 var parent = this.getParent();
@@ -40,47 +41,128 @@ odoo.define('sale.distributor', function (require) {
                     var index = array_child_ids.indexOf(this.res_id)
                 }
                 var total_length = array_child_ids.length - 1
+                var readonly = _.isNumber(options.res_id) && options.readonly;
                 if ('buttons' in this) {
-                    if(total_length != index){
-                        this.buttons.splice(this.buttons.length, 0, {
-                            classes: "fa fa-chevron-right btn-primary button-next-form-dialog",
-                            click: function () {
-                                this.on_click_form_dialog_next();
-                            },
-                        });
-                    }
-                    if(index != 0){
-                        this.buttons.splice(this.buttons.length, 1, {
-                            classes: "fa fa-chevron-left btn-primary button-previous-form-dialog",
-                            click: function () {
-                                this.on_click_form_dialog_previous();
-                            },
-                        });
-                    }
-
+                    // Pager
                     if(this.res_id){
-                        this.buttons.splice(this.buttons.length, 3, {
+                        this.buttons.splice(this.buttons.length, 0, {
                             text: array_child_ids.length,
                             classes: "form_pager",
                         });
-                        this.buttons.splice(this.buttons.length, 4, {
+                        this.buttons.splice(this.buttons.length, 1, {
                             text: _t("/"),
                             classes: "form_pager",
                         });
-                        this.buttons.splice(this.buttons.length, 5, {
+                        this.buttons.splice(this.buttons.length, 2, {
                             text: index + 1,
                             classes: "form_pager",
                         });
                     }
+                    // Next & Previous Button
+                    if(total_length != index && readonly){
+                        this.buttons.splice(this.buttons.length, 3, {
+                            classes: "fa fa-chevron-right btn-primary button-next-form-dialog",
+                            click: function () {
+                                this.on_click_form_dialog_next_read();
+                            },
+                        });
+                    }
+                    if(index != 0 && readonly){
+                        this.buttons.splice(this.buttons.length, 4, {
+                            classes: "fa fa-chevron-left btn-primary button-previous-form-dialog",
+                            click: function () {
+                                this.on_click_form_dialog_previous_read();
+                            },
+                        });
+                    }
+
+                    if(total_length != index && !readonly && this.res_id){
+                        this.buttons.splice(this.buttons.length, 5, {
+                            text: _t("SAVE & NEXT"),
+                            classes: "btn-position btn-primary button-next-form-dialog",
+                            click: function () {
+                                this.on_click_form_dialog_next_edit();
+                            },
+                        });
+                    }
+                    if(index != 0 && !readonly && this.res_id){
+                        this.buttons.splice(this.buttons.length, 6, {
+                            text: _t("SAVE & PREVIOUS"),
+                            classes: "btn-position btn-primary button-previous-form-dialog",
+                            click: function () {
+                                this.on_click_form_dialog_previous_edit();
+                            },
+                        });
+                    }
+                    // Save & Close Button
+                    if(!readonly){
+                        this.buttons[0].text = (multi_select ? _t("Save & Close") : _t("Save & Close"));
+                        this.buttons[0].click = function () {this._save().then(this.close.bind(this));}
+                    }
                 }
             }
-            return res
+            // return res
         },
-        // Next Button
-        on_click_form_dialog_next: function(){
+        // Next Button Editable Mode
+        on_click_form_dialog_next_edit: function(){
             var options = this.options;
             var parent = this.getParent();
-            // this._save().then(this.close.bind(this));
+            var local_array = parent.model.localData[this.parentID];
+            // var array_child_ids = Object.keys(local_array._cache)
+            var array_child_ids = _.pluck(parent.renderer.state.data.order_line.data, 'res_id')
+            if (this.res_id){
+                // var index = array_child_ids.indexOf(this.res_id.toString())
+                var index = array_child_ids.indexOf(this.res_id)
+            }
+            else{
+                var index = array_child_ids.length
+            }
+            if (index === (array_child_ids.length - 1)){
+                this.close();
+            }else{
+                options.res_id = array_child_ids[index +1];
+                options.recordID = local_array._cache[options.res_id];
+                // this.form_view.trigger_up('open_record', {id: options.res_id})
+
+                var old_dialog = _.extend({}, this);
+                var new_dialog = new FormDialog.FormViewDialog(parent, options);
+                new_dialog.open();
+                new_dialog.opened().then(function(){
+                    old_dialog._save().then(old_dialog.close());
+                })
+            }
+        },
+        // Previous Button Editable Mode
+        on_click_form_dialog_previous_edit: function(){
+            var options = this.options;
+            var parent = this.getParent();
+            var local_array = parent.model.localData[this.parentID];
+            // var array_child_ids = Object.keys(local_array._cache)
+            var array_child_ids = _.pluck(parent.renderer.state.data.order_line.data, 'res_id')
+            if (this.res_id){
+                // var index = array_child_ids.indexOf(this.res_id.toString())
+                var index = array_child_ids.indexOf(this.res_id)
+            }
+            else{
+                var index = array_child_ids.length -1
+            }
+            if (index === 0){
+                this.close();
+            }else{
+                options.res_id = array_child_ids[index - 1];
+                options.recordID = local_array._cache[options.res_id];
+                var old_dialog = _.extend({}, this);
+                var new_dialog = new FormDialog.FormViewDialog(parent, options);
+                new_dialog.open();
+                new_dialog.opened().then(function(){
+                    old_dialog._save().then(old_dialog.close());
+                })
+            }
+        },
+        // Next Button Readonly Mode
+        on_click_form_dialog_next_read: function(){
+            var options = this.options;
+            var parent = this.getParent();
             var local_array = parent.model.localData[this.parentID];
             // var array_child_ids = Object.keys(local_array._cache)
             var array_child_ids = _.pluck(parent.renderer.state.data.order_line.data, 'res_id')
@@ -104,8 +186,8 @@ odoo.define('sale.distributor', function (require) {
                 })
             }
         },
-        // Previous Button
-        on_click_form_dialog_previous: function(){
+        // Previous Button Readonly Mode
+        on_click_form_dialog_previous_read: function(){
             var options = this.options;
             var parent = this.getParent();
             var local_array = parent.model.localData[this.parentID];
