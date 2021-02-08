5225d85b0fe0f8909f0e076721bfc570b86b18b8
cheten.ravaliya <chetan.ravaliya@bistasolutions.com>
2021-02-08 14:29:01 +0530

    [IMP] Order line button validation, order status ribbon and show code wise product

--- sale_distributor/models/product.py
+++ sale_distributor/models/product.py
@@ -15,14 +15,27 @@ from odoo.osv import expression
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    def name_get(self):
+        if self._context.get('split_line_product'):
+            result = []
+            for prod in self:
+                default_code = ''
+                if prod.product_tmpl_id.default_code:
+                    default_code = "[%s] " % (prod.product_tmpl_id.default_code)
+                name = "%s" % (default_code)
+                if not name:
+                    name = '%s' % prod.product_tmpl_id.name or ''
+                result.append((prod.id, name))
+            return result
+        return super(ProductProduct, self).name_get()
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     product_pack_line = fields.One2many("product.pack.uom", "product_tmpl_id",
-    									string="Product Pack's",
-    									domain=[('is_auto_created','!=',True)],
-    									help="Define product packs.")
+                                        string="Product Pack's",
+                                        domain=[('is_auto_created','!=',True)],
+                                        help="Define product packs.")
     phone_number = fields.Char(string="Phone Number")
     dropship_selection = fields.Selection([('Yes', 'Yes'), ('No', 'No')], string='Dropships')
     dropship_fee = fields.Float(string="Dropship Fee")
@@ -47,13 +60,13 @@ class ProductTemplate(models.Model):
     @api.model
     def create(self, vals):
         if vals.get('name'):
-        	pack_auto_line = (0, 0, {
-	                 'quantity': 1.0,
-	                 'is_auto_created': True})
-        	if vals.get('product_pack_line'):
-	            vals['product_pack_line'].append(pack_auto_line)
-	        else:
-	        	vals['product_pack_line'] = [pack_auto_line]
+            pack_auto_line = (0, 0, {
+                     'quantity': 1.0,
+                     'is_auto_created': True})
+            if vals.get('product_pack_line'):
+                vals['product_pack_line'].append(pack_auto_line)
+            else:
+                vals['product_pack_line'] = [pack_auto_line]
         return super(ProductTemplate, self).create(vals)
 
 class ProductSupplierinfo(models.Model):
@@ -67,38 +80,40 @@ class ProductSupplierinfo(models.Model):
         self.active = False if self.ignore_cost else True
 
 class ProductPackUom(models.Model):
-	_name="product.pack.uom"
-	_description = "Product Pack Uom"
-	_order="product_tmpl_id, name"
+    _name="product.pack.uom"
+    _description = "Product Pack Uom"
+    _order="product_tmpl_id, name"
 
-	name = fields.Char("Name")
-	product_id = fields.Many2one("product.product",'Product')
-	product_tmpl_id = fields.Many2one("product.template","Product Template")
-	quantity = fields.Float("Quantity", digits='Product Unit of Measure')
-	is_auto_created = fields.Boolean("Auto Created", default=False)
-	price = fields.Float("Price")
+    name = fields.Char("Name")
+    product_id = fields.Many2one("product.product",'Product')
+    product_tmpl_id = fields.Many2one("product.template","Product Template")
+    quantity = fields.Float("Quantity", digits='Product Unit of Measure')
+    is_auto_created = fields.Boolean("Auto Created", default=False)
+    price = fields.Float("Price")
 
-	def name_get(self):
-		result = []
-		for prod in self:
-			default_code = ''
-			if prod.product_tmpl_id.default_code:
-				default_code = "[%s] " % (prod.product_tmpl_id.default_code)
-			name = "%s%s" % (default_code, prod.product_tmpl_id.name)
-			if prod.name:
-				name = name + '-%s' % prod.name or ''
-			result.append((prod.id, name))
-		return result
+    def name_get(self):
+        result = []
+        for prod in self:
+            default_code = ''
+            if prod.product_tmpl_id.default_code:
+                default_code = "[%s] " % (prod.product_tmpl_id.default_code)
+            name = "%s" % (default_code)
+            if prod.name:
+                name = name + '-%s' % prod.name or ''
+            if not prod.name:
+                name = '%s' % prod.product_tmpl_id.name or ''
+            result.append((prod.id, name))
+        return result
 
-	@api.model
-	def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
-		args = args or []
-		if operator == 'ilike' and not (name or '').strip():
-			domain = []
-		else:
-			domain = ['|','|', ('name', 'ilike', name), ('product_tmpl_id.default_code', 'ilike', name), ('product_tmpl_id.name', 'ilike', name)]
-		sat_code_ids = self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
-		return models.lazy_name_get(self.browse(sat_code_ids).with_user(name_get_uid))
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        if operator == 'ilike' and not (name or '').strip():
+            domain = []
+        else:
+            domain = ['|','|', ('name', 'ilike', name), ('product_tmpl_id.default_code', 'ilike', name), ('product_tmpl_id.name', 'ilike', name)]
+        sat_code_ids = self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
+        return models.lazy_name_get(self.browse(sat_code_ids).with_user(name_get_uid))
 
 
 class ProductDimensionLine(models.Model):
