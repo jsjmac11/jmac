be27d5ff6d4e69861c658dbaa29e117d7113ddde
Jagdish Panchal <jagdish.panchal@bistasolutions.com>
2021-10-21 17:41:30 +0530

    [IMP]Improved code for fix issue in sh

--- amazon_ept/models/product_shipping_template.py
+++ amazon_ept/models/product_shipping_template.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 
-class amazon_product_shipping_template(models.Model):
+class AmazonProductShippingTemplate(models.Model):
     _name = "amazon.product.shipping.template"
     _description = 'amazon.product.shipping.template'
 
