8f056708dc94a6384b4dffe83c477bfb31b0f7fb
Diogo Duarte <dduarte@odoogap.com>
2022-12-02 12:09:15 +0000

    [IMP] added central module

---
+++ jmac/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'JMAC',
+    'version': '16.0.0.1',
+    'category': 'Sales/Sales',
+    'description': "JMAC Central Module",
+    'depends': [
+        'sale_stock',  'purchase'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
